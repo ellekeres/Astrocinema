7bbd8bbd47eab86a54b67bbf6fa45998b69b8c91
ellekeres <113652352+ellekeres@users.noreply.github.com>
2022-11-27 06:36:48 +0000

    started second slideshow and started horoscope page

--- script.js
+++ script.js
@@ -10,7 +10,8 @@ function lastUpdated(){
   document.getElementById("lm").innerHTML = "Last Modified: " + d.toDateString();
 }
 
-// Movie catalog code for slideshow
+/*
+// movie catalog code for slideshow
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -38,9 +39,45 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+}*/
+
+var slideshow1 = document.getElementById("slideshow1");
+slideshow1.currentSlideIndex = 1;
+showSlides(slideshow1.currentSlideIndex, slideshow1);
+
+var slideshow2 = document.getElementById("slideshow2");
+slideshow2.currentSlideIndex = 1;
+showSlides(slideshow2.currentSlideIndex, slideshow2);
+
+
+function plusSlides(n, slideshow) {
+  showSlides(slideshow.currentSlideIndex += n, slideshow);
 }
 
-/* changing the color of modalities and elements with buttons */
+function currentSlide(n, slideshow) {
+  showSlides(slideshow.currentSlideIndex = n, slideshow);
+}
+
+function showSlides(n, slideshow) {
+  
+
+
+  var i;
+  var slides = slideshow.getElementsByClassName("movieSlides");
+  var dots = slideshow.getElementsByClassName("dot");
+  if (n > slides.length) {slideshow.currentSlideIndex = 1}    
+  if (n < 1) {slideshow.currentSlideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideshow.currentSlideIndex-1].style.display = "block";  
+  dots[slideshow.currentSlideIndex-1].className += " active";
+}
+
+// changing the color of modalities and elements with buttons 
 function changeColor(color, type){
   
   let test = document.getElementsByClassName(type);  
@@ -50,3 +87,13 @@ function changeColor(color, type){
 }
   
 }
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon 
+function myFunction() {
+  var x = document.getElementById("topnav");
+  if (x.className === "n") {
+    x.className += " responsive";
+  } else {
+    x.className = "n";
+  }
+} */
